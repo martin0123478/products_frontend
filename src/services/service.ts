23ccc67c5460d232ab668b8016e9250de99d731a
martin0123478 <martin_agui97@outlook.es>
2024-11-03 21:22:55 -0600

    feat:update product

--- src/services/service.ts
+++ src/services/service.ts
@@ -71,7 +71,10 @@ export async function updateProduct(data: productData, id: Product['id']) {
             price: parse(NumberSchema, data.price),
             availability: toBoolean(data.availability.toString())
         })
-        console.log(result)
+        if (result.success) {
+            const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+            await axios.put(url, result.output)
+        }
     } catch (error) {
         console.log(error)
     }
